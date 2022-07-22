07b3ec4f86413fec07332e061eff6f16f1d0f49e
Mike Korotych <mixakievchanen@gmail.com>
2022-07-23 01:28:01 +0300

    anim left

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const read = document.querySelectorAll("#read");
 const nav = document.querySelector(".nav");
 const navHeight = nav.getBoundingClientRect().height;
 const header = document.querySelector(".section-1");
+const allSections = document.querySelectorAll("section");
+const overlay = document.querySelector(".overlay");
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
 };
@@ -20,6 +21,60 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Reveal sections
+const revealSections = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSections, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+//handleHover
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const lastLink = link.closest(".nav").querySelector(".r");
+    const siblings = link.closest(".nav").querySelectorAll(".nav-link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = 1;
+    lastLink.style.opacity = 1;
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Page navigation
+
+document
+  .querySelector(".nav-links__wrapper")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log(e.target);
+
+    if (e.target.classList.contains("nav-link")) {
+      const id = e.target.getAttribute("href");
+      console.log(id);
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+  });
+
+/////////////////////////////////////////////////////////////////////////
 anime({
   targets: navLinks,
   translateX: [50, 0],
@@ -32,103 +87,65 @@ anime({
   opacity: [0, 1],
 });
 
-anime({
-  targets: "#demo",
-  value: [0, 100],
-  // duration: 4000,
-  round: 1,
-  borderRadius: ["0px", "36px"],
-  opacity: 0.5,
-  easing: "easeInOutQuad",
+const confirmAnim = anime({
+  targets: ".confirm span",
+  translateX: [-370, 0],
+  duration: 2000,
+});
+const confirm = document.querySelector(".confirm");
+confirm.addEventListener("click", (e) => {
+  confirmAnim.restart();
 });
 
-btns.forEach(function (btn) {
-  btn.addEventListener("mouseenter", function (e) {
-    console.log("mouseenter");
-    // LENS
-    if (e.target) {
-      anime({
-        targets: "#lens3 path",
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: "easeInOutSine",
-        duration: 900,
-        opacity: [0, 1],
-        rotate: "1turn",
-        // transformOrigin: "center",
-        // transformBox: "fill-box",
-        // rotate: 360,
-        delay: function (el, i) {
-          return i * 250;
-        },
-      });
-      // READ
-      anime({
-        targets: read,
-        translateX: [-11, 0],
-        duration: 400,
-        easing: "easeInOutQuad",
-      });
-    }
-  });
-});
-btns.forEach(function (btn) {
-  btn.addEventListener("mouseout", function (e) {
-    console.log("mouseout");
-
-    // LENS
-    anime({
-      targets: "#lens3 path",
-      strokeDashoffset: [anime.setDashoffset, 0],
-      easing: "linear",
-      duration: 250,
-      opacity: [0, 1],
-      direction: "reverse",
-      // delay: 500,
-      delay: function (el, i) {
-        return i * 0;
-      },
-    });
-    // READ
-    anime({
-      targets: read,
-      translateX: [0, -11],
-      duration: 400,
-      // delay: 500,
-      easing: "easeInOutQuad",
-    });
-  });
-});
-
-// anime({
-//   targets: "#lens3 path",
+// const btnAnimitem_1 = anime({
+//   targets: `#black-round-${num} path`,
 //   strokeDashoffset: [anime.setDashoffset, 0],
-//   easing: "linear",
-//   duration: 0,
-//   opacity: [1, 0],
+//   easing: "easeInOutQuad",
+//   autoplay: false,
+//   duration: 200,
 //   delay: function (el, i) {
-//     return i * 0;
+//     return i * 50;
 //   },
 // });
 
-// anime({
-//   targets: read,
-//   translateX: [20, -11],
-//   duration: 300,
-//   opacity: [0, 1],
-//   easing: "easeInOutQuad",
+const itemWrapper = document.querySelector(".section-2__item--wrapper");
+const items = document.querySelectorAll(".section-2__item");
+
+// itemWrapper.addEventListener("click", (e) => {
+//   const clicked = e.target.closest(".btn");
+
+//   if (!clicked) return;
+
+//   // btns.forEach((i) => btnAnimation.restart());
+//   btnAnim.restart();
+
+//   console.log(btnAnim, clicked);
 // });
 
-anime({
-  targets: "#black-round path",
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: "easeInOutQuad",
-  // easing: "easeInOutSine",
+items.forEach((i) =>
+  i.addEventListener("click", (e) => {
+    e.preventDefault();
+    const clicked = e.target.closest(".btn");
+    if (!clicked) return;
+    const num = clicked.dataset.number.slice(-1);
+    console.log(num);
 
-  // easing: "linear",
-  // direction: "reverse",
-  duration: 200,
-
-  delay: function (el, i) {
-    return i * 50;
-  },
-});
+    if (i.classList.contains(clicked.dataset.number)) {
+      // i.classList.toggle("active");
+      // overlay.classList.toggle("hidden");
+      const btnAnimitem_1 = anime({
+        targets: `#black-round-${num} path`,
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: "easeInOutSine",
+        // easing: "easeInOutQuad",
+        // easing: "easeInOutExpo",
+        autoplay: false,
+        duration: 200,
+        delay: function (el, i) {
+          return i * 70;
+        },
+      });
+      btnAnimitem_1.restart();
+    }
+  })
+);
